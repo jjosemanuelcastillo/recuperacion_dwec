3189a6fa7d0e10c826a835248599c20706b8259b
José Manuel Castillo Calle <josemanuelcastillocalle83@gmail.com>
2025-03-21 20:33:33 +0100

    Ejercicio 4.24

---
+++ Boletin_DOM/Ejercicio_4.24/Ejercicio.js
@@ -0,0 +1,49 @@
+function insertar() {
+    //cogemos el id deldiv del html
+    let lista = document.getElementById("listaTexto");
+    //Recogemos el valor introducido del campo de texto 
+    let texto = document.getElementById("textoIntroducido").value;
+
+    //Creamos elemento p para añadirlo al html
+    let p = document.createElement("p");
+    //Agregamos el texto recogido anteriormente al parrafo
+    p.innerHTML = texto;
+    //Creación de los botones
+    let actualizar = document.createElement("button")
+    let borrar = document.createElement("button")
+
+    //Texto para los botones
+    actualizar.textContent = "Actualizar";
+    borrar.textContent = "Borrar";
+
+    //introducimos las etiquetas creadas anteriormente a la lista que es el div del html
+    lista.appendChild(p);
+    lista.appendChild(actualizar);
+    lista.appendChild(borrar);
+
+    //evento onclick para actualizar
+    actualizar.onclick = () => {
+        let textoNuevo = prompt("Nuevo texto", p.textContent);
+        if (textoNuevo !== null) {
+            p.innerHTML = textoNuevo;
+        }
+
+    };
+    //EVento onclick para borrar
+    borrar.onclick = () => {
+        p.remove();
+        borrar.remove();
+        actualizar.remove();
+    };
+
+
+}
+
+function cambiar() {
+    let div = document.getElementById("listaTexto");
+    let parrafos = div.querySelectorAll("p");
+    let primerParrafo = parrafos[0];
+    let ultimoParrafo = parrafos[parrafos.length - 1];
+    div.insertBefore(ultimoParrafo , primerParrafo);
+    div.appendChild(primerParrafo);
+}
