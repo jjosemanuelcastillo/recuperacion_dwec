7238b131adf29f77f918207ea874c618ab2752b0
José Manuel Castillo Calle <josemanuelcastillocalle83@gmail.com>
2025-05-10 18:29:44 +0200

    Cambios

--- Angular/Ejemplos/proyecto001/src/app/app.module.ts
+++ Angular/Ejemplos/proyecto001/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DadoComponentComponent } from './dado-component/dado-component.component';
+import { ComponenteEnLineaComponent } from './componente-en-linea/componente-en-linea.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DadoComponentComponent
+    DadoComponentComponent,
+    ComponenteEnLineaComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/Ejemplos/proyecto001/src/app/componente-en-linea/componente-en-linea.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-en-linea',
+  standalone: false,
+  template: `
+    <h1>
+      componente-en-linea works!
+</h1>
+  `,
+  styles: `
+  h1{
+    color: blue;
+  }
+  `
+})
+export class ComponenteEnLineaComponent {
+
+}
