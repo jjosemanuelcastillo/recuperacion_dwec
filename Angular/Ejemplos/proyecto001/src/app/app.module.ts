dce44f80e50af73a6d8a9a3cb63bfc2068d538a8
José Manuel Castillo Calle <josemanuelcastillocalle83@gmail.com>
2025-05-10 19:23:37 +0200

    Cambios

--- Angular/Ejemplos/proyecto001/src/app/app.module.ts
+++ Angular/Ejemplos/proyecto001/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DadoComponentComponent } from './dado-component/dado-component.component';
-import { ComponenteEnLineaComponent } from './componente-en-linea/componente-en-linea.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DadoComponentComponent,
-    ComponenteEnLineaComponent
   ],
   imports: [
     BrowserModule,
